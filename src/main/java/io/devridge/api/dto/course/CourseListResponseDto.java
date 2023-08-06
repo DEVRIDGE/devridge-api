86aa19d56c75808c893a64a71467873cc3f08256
Woosung_Kim <sibu2005@naver.com>
2023-08-06 18:23:54 +0900

    feat : index 번호 추가 - 프론트 요구사항

--- src/main/java/io/devridge/api/dto/course/CourseListResponseDto.java
+++ src/main/java/io/devridge/api/dto/course/CourseListResponseDto.java
@@ -1,10 +1,12 @@
 package io.devridge.api.dto.course;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.devridge.api.domain.course.Course;
 import io.devridge.api.domain.course.CourseType;
 import lombok.Getter;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Getter
@@ -12,19 +14,27 @@ public class CourseListResponseDto {
     private List<List<CourseDto>> courseList;
 
     public CourseListResponseDto(List<Course> courseList) {
+
         Collection<List<CourseDto>> courseListCollection = courseList.stream()
-                .map(CourseDto::new)
+                .map((course) -> new CourseDto(course))
                 .collect(Collectors.groupingBy(CourseDto::getTurn, TreeMap::new, Collectors.toList())).values();
 
         List<List<CourseDto>> courseListAddEmptyList = new ArrayList<>();
         CourseType previousCourseType = null;
 
-        for (List<CourseDto> courseDto : courseListCollection) {
-            if(previousCourseType == CourseType.SKILL && courseDto.get(0).getType().equals(CourseType.SKILL)) {
+        AtomicInteger indexCounter = new AtomicInteger(0);
+        for (List<CourseDto> courseDtoList : courseListCollection) {
+
+            if(previousCourseType == CourseType.SKILL && courseDtoList.get(0).getType().equals(CourseType.SKILL)) {
                 courseListAddEmptyList.add(Collections.emptyList());
+                indexCounter.getAndIncrement();
             }
-            courseListAddEmptyList.add(courseDto);
-            previousCourseType = courseDto.get(0).getType();
+
+            for (CourseDto courseDto : courseDtoList) {
+                courseDto.setIndex(indexCounter.getAndIncrement());
+            }
+            courseListAddEmptyList.add(courseDtoList);
+            previousCourseType = courseDtoList.get(0).getType();
         }
 
         this.courseList = courseListAddEmptyList;
@@ -32,9 +42,11 @@ public class CourseListResponseDto {
 
     @Getter
     public static class CourseDto {
+        private int index;
         private final Long id;
         private final String name;
         private final CourseType type;
+        @JsonIgnore
         private final int turn;
 
         public CourseDto(Course course) {
@@ -42,6 +54,10 @@ public class CourseListResponseDto {
             this.name = course.getName();
             this.type = course.getType();
             this.turn = course.getTurn();
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
         }
     }
 }
