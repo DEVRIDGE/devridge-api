8da0e169aca2acc6fc96e781f34f21c51779fe68
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-08-06 19:22:13 +0900

    feat: CourseDetailRepository @Param 추가

--- src/main/java/io/devridge/api/domain/course/CourseDetailRepository.java
+++ src/main/java/io/devridge/api/domain/course/CourseDetailRepository.java
@@ -2,6 +2,7 @@ package io.devridge.api.domain.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,6 +13,6 @@ public interface CourseDetailRepository extends JpaRepository<CourseDetail, Long
             "LEFT JOIN EmploymentSkillCourseDetail escd ON cd.id = escd.courseDetail.id " +
             "LEFT JOIN escd.employmentSkill es ON escd.employmentSkill.id = es.id " +
             "LEFT JOIN es.employmentInfo ei ON es.employmentInfo.id = ei.id " +
-            "WHERE cd.course.id = ?1 AND ei.company.id = ?2 AND ei.job.id = ?3")
-    List<CourseDetail> getCourseDetailListByCourseIdAndCompanyIdAndJobId(long courseId, long companyId, long jobId);
+            "WHERE cd.course.id = :courseId AND ei.company.id = :companyId AND ei.job.id = :jobId")
+    List<CourseDetail> getCourseDetailListByCourseIdAndCompanyIdAndJobId(@Param("courseId")long courseId,@Param("companyId") long companyId,@Param("jobId") long jobId);
 }
