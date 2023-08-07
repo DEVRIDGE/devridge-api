ee27ffab8e374dfe43f61fb8956e99a85ed8b38c
Habin Song <boulce.email@gmail.com>
2023-08-07 14:27:53 +0900

    feat: CourseNotFoundExceiption 추가, CourseDetailService -> CourseService로 옮김, CourseDetailController -> CourseController로 옮김

--- src/main/java/io/devridge/api/handler/CourseExceptionHandler.java
+++ src/main/java/io/devridge/api/handler/CourseExceptionHandler.java
@@ -2,6 +2,7 @@ package io.devridge.api.handler;
 
 import io.devridge.api.dto.common.ApiResponse;
 import io.devridge.api.handler.ex.CompanyJobNotFoundException;
+import io.devridge.api.handler.ex.CourseNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,12 @@ public class CourseExceptionHandler {
     @ExceptionHandler(CompanyJobNotFoundException.class)
     public ResponseEntity<ApiResponse<Object>> handleCompanyJobNotFoundException(CompanyJobNotFoundException exception) {
         log.error("CompanyJobNotFoundException = {}", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error(exception.getMessage()));
+    }
+
+    @ExceptionHandler(CourseNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleCourseNotFoundException(CourseNotFoundException exception) {
+        log.error("CourseNotFoundException = {}", exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error(exception.getMessage()));
     }
 }
--- src/main/java/io/devridge/api/service/CourseService.java
+++ src/main/java/io/devridge/api/service/CourseService.java
@@ -2,9 +2,13 @@ package io.devridge.api.service;
 
 import io.devridge.api.domain.company_job.CompanyJobRepository;
 import io.devridge.api.domain.course.Course;
+import io.devridge.api.domain.course.CourseDetail;
+import io.devridge.api.domain.course.CourseDetailRepository;
 import io.devridge.api.domain.course.CourseRepository;
+import io.devridge.api.dto.CourseDetailResponseDto;
 import io.devridge.api.dto.course.CourseListResponseDto;
 import io.devridge.api.handler.ex.CompanyJobNotFoundException;
+import io.devridge.api.handler.ex.CourseNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +17,26 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class CourseService {
-
-    private final CourseRepository courseJpaRepository;
+    private final CourseRepository courseRepository;
     private final CompanyJobRepository companyJobRepository;
-
+    private final CourseDetailRepository courseDetailRepository;
     @Transactional(readOnly = true)
     public CourseListResponseDto getCourseList(long companyId, long jobId) {
         validateCompanyJobExists(companyId, jobId);
 
-        List<Course> courseList = courseJpaRepository.getCourseListByJob(jobId);
+        List<Course> courseList = courseRepository.getCourseListByJob(jobId);
 
         return new CourseListResponseDto(courseList);
+    }
+    @Transactional(readOnly = true)
+    public CourseDetailResponseDto getCourseDetailList(long courseId, long companyId, long jobId) {
+        validateCompanyJobExists(companyId, jobId);
+
+        List<CourseDetail> courseDetailList = courseDetailRepository.getCourseDetailListByCourseIdAndCompanyIdAndJobId(courseId, companyId, jobId);
+        Course courseName = courseRepository.findById(courseId).orElseThrow(() -> new CourseNotFoundException("해당하는 코스가 없습니다."));
+
+
+        return new CourseDetailResponseDto(courseName.getName(), courseDetailList);
     }
 
     private void validateCompanyJobExists(long companyId, long jobId) {
