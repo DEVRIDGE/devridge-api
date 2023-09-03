ab503b5945e7d0e822dbc96ad0f09953be340241
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-03 23:11:16 +0900

    feat: newdomain/Course  도메인 추가

--- src/main/java/io/devridge/api/newdomain/roadmap/Course.java
+++ src/main/java/io/devridge/api/newdomain/roadmap/Course.java
@@ -1,18 +1,36 @@
 package io.devridge.api.newdomain.roadmap;
 
+import io.devridge.api.newdomain.BaseTimeEntity;
+import io.devridge.api.newdomain.companyinfo.Job;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "job_service")
+@Table(name = "course")
 @Entity
-public class Course {
+public class Course extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private Long id;
+
+    @Column(name = "course_name")
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "course_type")
+    private String type;
+
+    @Column(name = "course_order")
+    private Integer order;
+
+    @JoinColumn(name = "job_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Job job;
 }
