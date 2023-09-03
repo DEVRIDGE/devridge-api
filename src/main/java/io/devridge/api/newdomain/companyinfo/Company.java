4b4f9d40e6d87eabd2737f75e598215e0b633761
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-03 22:35:13 +0900

    feat: newdomain/Company 도메인 추가

--- src/main/java/io/devridge/api/newdomain/companyinfo/Company.java
+++ src/main/java/io/devridge/api/newdomain/companyinfo/Company.java
@@ -1,4 +1,26 @@
 package io.devridge.api.newdomain.companyinfo;
 
-public class Company {
+import io.devridge.api.newdomain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "comapny")
+@Entity
+public class Company extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_id")
+    private Long id;
+
+    @Column(name = "company_name")
+    private String name;
+
 }
