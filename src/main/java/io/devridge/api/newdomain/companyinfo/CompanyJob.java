64b7fbdfa0f6549f1cf6e4a8b6419dcf8bef4256
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-03 22:41:42 +0900

    feat: newdomain/CompanyJob  도메인 추가

--- src/main/java/io/devridge/api/newdomain/companyinfo/CompanyJob.java
+++ src/main/java/io/devridge/api/newdomain/companyinfo/CompanyJob.java
@@ -1,4 +1,28 @@
 package io.devridge.api.newdomain.companyinfo;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "company_job")
+@Entity
 public class CompanyJob {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_job_id")
+    private Long id;
+
+    @JoinColumn(name = "job_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Job job;
+
+    @JoinColumn(name = "company")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
 }
