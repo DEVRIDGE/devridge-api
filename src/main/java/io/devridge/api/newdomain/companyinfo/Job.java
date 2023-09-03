5acd878340513e6125a15756ede59b041031d264
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-03 22:33:13 +0900

    feat: newdomain/Job 도메인 추가

--- src/main/java/io/devridge/api/newdomain/companyinfo/Job.java
+++ src/main/java/io/devridge/api/newdomain/companyinfo/Job.java
@@ -1,4 +1,25 @@
 package io.devridge.api.newdomain.companyinfo;
 
-public class Job {
+import io.devridge.api.newdomain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "job")
+@Entity
+public class Job extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_id")
+    private Long id;
+
+    @Column(name = "job_name")
+    private String name;
 }
--- src/main/java/io/devridge/api/newdomain/user/UserRoadmap.java
+++ src/main/java/io/devridge/api/newdomain/user/UserRoadmap.java
@@ -1,4 +1,35 @@
 package io.devridge.api.newdomain.user;
 
-public class UserRoadmap {
+import io.devridge.api.newdomain.BaseTimeEntity;
+import io.devridge.api.newdomain.roadmap.Roadmap;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user_roadmap")
+@Entity
+public class UserRoadmap extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_roadmap_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "roadmap_study_status")
+    private String studyStatus;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @JoinColumn(name = "roadmap_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Roadmap roadmap;
 }
