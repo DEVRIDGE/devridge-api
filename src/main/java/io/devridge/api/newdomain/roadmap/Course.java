48ea854fc821d59086c8869e6d2761e9e7c3315a
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-03 22:56:02 +0900

    feat: newdomain/CompanyRequiredAbility  도메인 추가

--- src/main/java/io/devridge/api/newdomain/roadmap/Course.java
+++ src/main/java/io/devridge/api/newdomain/roadmap/Course.java
@@ -1,4 +1,18 @@
 package io.devridge.api.newdomain.roadmap;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "job_service")
+@Entity
 public class Course {
 }
