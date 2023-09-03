cbf30eba6c3aafa0e9a681557862a37470fa6fcb
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-03 23:25:14 +0900

    feat: newdomain/Post  도메인 추가

--- src/main/java/io/devridge/api/newdomain/post/Post.java
+++ src/main/java/io/devridge/api/newdomain/post/Post.java
@@ -1,4 +1,33 @@
 package io.devridge.api.newdomain.post;
 
-public class Post {
+import io.devridge.api.newdomain.BaseTimeEntity;
+import io.devridge.api.newdomain.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "post")
+@Entity
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "post_type")
+    private String type;
+
+    @Column(name = "post_content")
+    private String content;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
