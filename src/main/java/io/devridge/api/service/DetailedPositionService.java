ac99190f3c80befff6e05da12eed56b4933ae95c
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-07 20:25:12 +0900

    feat: DetailedPositionController, DetailedPositionService, DetailedPositionRepository 클래스 생성

---
+++ src/main/java/io/devridge/api/service/DetailedPositionService.java
@@ -0,0 +1,7 @@
+package io.devridge.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DetailedPositionService {
+}
---
+++ src/main/java/io/devridge/api/web/DetailedPositionController.java
@@ -0,0 +1,20 @@
+package io.devridge.api.web;
+
+import io.devridge.api.dto.common.ApiResponse;
+import io.devridge.api.service.DetailedPositionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DetailedPositionController {
+    private DetailedPositionService detailedPositionService;
+
+    @GetMapping("/detailedPositions")
+    public ResponseEntity<ApiResponse<Object>> detailedPositionList() {
+
+        return null;
+    }
+}
