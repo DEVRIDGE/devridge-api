4c6f5ce75f43f12a64588dc692a26ef4f32e3bee
Woosung_Kim <sibu2005@naver.com>
2023-09-07 11:40:22 +0900

    feat : token 도메인 추가

---
+++ src/main/java/io/devridge/api/domain/token/TokenRepository.java
@@ -0,0 +1,10 @@
+package io.devridge.api.domain.token;
+
+import io.devridge.api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByUser(User user);
+}
