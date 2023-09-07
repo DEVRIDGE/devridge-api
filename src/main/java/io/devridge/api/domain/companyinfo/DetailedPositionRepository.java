ff71d2b148a752e585b227c0a101a412f0fbd129
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-07 20:41:49 +0900

    feat: DetailedPositionRepository 쿼리메소드 추가

--- src/main/java/io/devridge/api/domain/companyinfo/DetailedPositionRepository.java
+++ src/main/java/io/devridge/api/domain/companyinfo/DetailedPositionRepository.java
@@ -1,6 +1,15 @@
 package io.devridge.api.domain.companyinfo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DetailedPositionRepository extends JpaRepository<DetailedPosition, Long> {
+
+    @Query("select dp from DetailedPosition dp " +
+            "JOIN JobDetailedPosition jdp ON jdp.detailedPosition.id = dp.id " +
+            "where dp.company.id = : companyId and jdp.job.id = :jobId")
+    List<DetailedPosition> findByCompanyIdAndJobId(@Param("companyId") long companyId, @Param("jobId") long jobId);
 }
--- src/main/java/io/devridge/api/service/DetailedPositionService.java
+++ src/main/java/io/devridge/api/service/DetailedPositionService.java
@@ -1,7 +1,13 @@
 package io.devridge.api.service;
 
+import io.devridge.api.domain.companyinfo.DetailedPositionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DetailedPositionService {
+    private final DetailedPositionRepository detailedPositionRepository;
+
+    //TODO DetailedPositionDto 만들고 서비스 로직 만들어야함
 }
