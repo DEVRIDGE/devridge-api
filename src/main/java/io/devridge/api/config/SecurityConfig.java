cb3b7682095bb642b26e2aa88b9c5823beb93b65
Woosung_Kim <sibu2005@naver.com>
2023-09-08 22:46:27 +0900

    feat : 인증 로직 추가 및 시큐리티 설정

--- src/main/java/io/devridge/api/config/SecurityConfig.java
+++ src/main/java/io/devridge/api/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package io.devridge.api.config;
 import io.devridge.api.config.auth.OAuth2FailHandler;
 import io.devridge.api.config.auth.OAuth2SuccessHandler;
 import io.devridge.api.config.auth.OAuth2UserService;
+import io.devridge.api.config.filter.CustomAuthenticationEntryPoint;
+import io.devridge.api.config.filter.CustomSecurityFilterManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -25,6 +27,8 @@ public class SecurityConfig {
     private final OAuth2UserService oauth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final OAuth2FailHandler oAuth2FailHandler;
+    private final CustomSecurityFilterManager customSecurityFilterManager;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -42,13 +46,14 @@ public class SecurityConfig {
                     loginConfigurer.failureHandler(oAuth2FailHandler);
                 })
                 .exceptionHandling(config -> {
-//                    config.authenticationEntryPoint(authenticationEntryPointResponseHandler);
+                    config.authenticationEntryPoint(customAuthenticationEntryPoint);
 //                    config.accessDeniedHandler(this::accessDeniedResponseHandler);
                 })
                 .authorizeRequests(requests -> requests.anyRequest().permitAll())
-
+                .apply(customSecurityFilterManager).and()
                 .build();
     }
+
 
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -60,18 +65,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-//    private void authenticationEntryPointResponseHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
-//        log.error("request.getRequestURI() = {}, ", request.getRequestURI());
-//        log.error("AuthenticationException = {}", exception.getMessage());
-//        CommonResponseHandler.handleException(response, "로그인이 필요합니다.", HttpStatus.UNAUTHORIZED);
-//    }
-//
-//    private void accessDeniedResponseHandler(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) {
-//        log.error("request.getRequestURI() = {}, ", request.getRequestURI());
-//        log.error("AccessDeniedException = {}", exception.getMessage());
-//        CommonResponseHandler.handleException(response, "권한이 없습니다.", HttpStatus.FORBIDDEN);
-//    }
 
     private void configureDevSettings(HttpSecurity http) throws Exception {
         if (isProfileActive("dev") || isProfileActive("test")) {
