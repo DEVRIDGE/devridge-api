b484614de34cc9e55314b99c932a534f6e9215be
Woosung_Kim <sibu2005@naver.com>
2023-09-08 16:34:49 +0900

    feat : 프론트 주소 협의

--- src/main/java/io/devridge/api/config/auth/OAuthSetting.java
+++ src/main/java/io/devridge/api/config/auth/OAuthSetting.java
@@ -2,6 +2,6 @@ package io.devridge.api.config.auth;
 
 
 public class OAuthSetting {
-    public static final String OAUTH2_LOGIN_AFTER_PAGE = "http://localhost:3000/login/callback";
-    public static final String OAUTH2_LOGIN_FAIL_PAGE = "http://localhost:3000/login/fail";
+    public static final String OAUTH2_LOGIN_AFTER_PAGE = "http://localhost:3000/devridge/loginRedirectPage";
+    public static final String OAUTH2_LOGIN_FAIL_PAGE = "http://localhost:3000/devridge/loginRedirectPage";
 }
