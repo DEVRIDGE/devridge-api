15da4d97e1f60e2b29af38a595c61ddb6e8f1284
Woosung_Kim <sibu2005@naver.com>
2023-09-08 22:21:49 +0900

    feat : 토큰 검증 로직

--- src/main/java/io/devridge/api/util/jwt/JwtTokenProvider.java
+++ src/main/java/io/devridge/api/util/jwt/JwtTokenProvider.java
@@ -2,16 +2,20 @@ package io.devridge.api.util.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.devridge.api.domain.user.User;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import io.devridge.api.util.jwt.exception.JwtExpiredException;
+import io.devridge.api.util.jwt.exception.JwtIdConversionException;
+import io.devridge.api.util.jwt.exception.JwtNotHaveIdException;
+import io.devridge.api.util.jwt.exception.JwtVerifyException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Slf4j
-@RequiredArgsConstructor
 @Component
 public class JwtTokenProvider implements TokenProvider {
 
@@ -38,40 +42,36 @@ public class JwtTokenProvider implements TokenProvider {
 
     @Override
     public Long verify(String token) {
-        return null;
-//        DecodedJWT decodedJWT = decodingToken(token);
-//        Claim JwtIdClaim = getIdByJwt(decodedJWT);
-//
-//        return convertIdClaimToLong(JwtIdClaim);
+        DecodedJWT decodedJWT = decodingToken(token);
+        Claim JwtIdClaim = getIdByJwt(decodedJWT);
+
+        return convertIdClaimToLong(JwtIdClaim);
     }
-//
-//    private DecodedJWT decodingToken(String token) {
-//        try {
-//            return JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(token);
-//        } catch (TokenExpiredException exception) {
-//            log.error("TokenExpiredException = {}", exception.getMessage());
-//            throw new JwtExpiredException();
-//        } catch (JWTVerificationException exception) {
-//            log.error("JWTVerificationException = {}", exception.getMessage());
-//            throw new JwtVerifyException();
-//        }
-//    }
-//
-//    private long convertIdClaimToLong(Claim idClaim) throws JwtIdConversionException {
-//        try {
-//            return idClaim.asLong();
-//        } catch (NumberFormatException exception) {
-//            log.error("idClaim = {}", idClaim);
-//            log.error("NumberFormatException  = {}", exception.getMessage());
-//            throw new JwtIdConversionException();
-//        }
-//    }
-//
-//    private Claim getIdByJwt(DecodedJWT decodedJWT) throws JwtNotHaveIdException {
-//        Claim idClaim = decodedJWT.getClaim("id");
-//        if (idClaim.isNull()) {
-//            throw new JwtNotHaveIdException();
-//        }
-//        return idClaim;
-//    }
+
+    private DecodedJWT decodingToken(String token) {
+        try {
+            return JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
+        } catch (TokenExpiredException exception) {
+            throw new JwtExpiredException(exception.getMessage());
+        } catch (JWTVerificationException exception) {
+            throw new JwtVerifyException(exception.getMessage());
+        }
+    }
+
+    private long convertIdClaimToLong(Claim idClaim) throws JwtIdConversionException {
+        try {
+            return idClaim.asLong();
+        } catch (NumberFormatException exception) {
+            String message = "idClaim : " + idClaim + ", NumberFormatException : " + exception.getMessage();
+            throw new JwtIdConversionException(message);
+        }
+    }
+
+    private Claim getIdByJwt(DecodedJWT decodedJWT) throws JwtNotHaveIdException {
+        Claim idClaim = decodedJWT.getClaim("id");
+        if (idClaim.isNull()) {
+            throw new JwtNotHaveIdException();
+        }
+        return idClaim;
+    }
 }
