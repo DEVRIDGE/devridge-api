cbc905a6ae25cce9de6aa78237021832c231728a
Woosung_Kim <sibu2005@naver.com>
2023-09-10 23:52:20 +0900

    feat : 순서 조건 추가

--- src/main/java/io/devridge/api/domain/roadmap/RoadmapRepository.java
+++ src/main/java/io/devridge/api/domain/roadmap/RoadmapRepository.java
@@ -11,6 +11,6 @@ public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
     @Query("SELECT r FROM Roadmap r " +
             "LEFT JOIN FETCH r.course c " +
             "WHERE r.companyInfo.id = :companyInfoId " +
-            "ORDER BY c.order")
+            "ORDER BY c.order, CASE c.type WHEN 'SKILL' THEN 0 WHEN 'CS' THEN 1 ELSE 2 END")
     List<Roadmap> getRoadmapsIncludingCoursesByCompanyInfoId(@Param("companyInfoId") Long companyInfoId);
 }
