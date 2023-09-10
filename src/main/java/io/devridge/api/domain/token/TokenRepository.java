65ccc8372987b450a4594a1657d6826131a67495
Woosung_Kim <sibu2005@naver.com>
2023-09-10 10:03:34 +0900

    feat : 토큰 재발행 컨트롤러, 서비스 및 예외처리

--- src/main/java/io/devridge/api/domain/token/TokenRepository.java
+++ src/main/java/io/devridge/api/domain/token/TokenRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByUser(User user);
+
+    Optional<Token> findByContent(String content);
 }
