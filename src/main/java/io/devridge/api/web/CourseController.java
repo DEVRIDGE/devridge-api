e265e6a761fd8242b134db7cda6c5c25e477e1ee
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-10 13:19:41 +0900

    feat: CourseController 클래스 courseDetailList 메소드에 detailedPosition 파라미터 추가

--- src/main/java/io/devridge/api/web/CourseController.java
+++ src/main/java/io/devridge/api/web/CourseController.java
@@ -1,17 +1,12 @@
 package io.devridge.api.web;
 
-import io.devridge.api.dto.CourseDetailResponseDto;
-import io.devridge.api.dto.CourseVideoResponseDto;
 import io.devridge.api.dto.common.ApiResponse;
 import io.devridge.api.dto.course.CourseListResponseDto;
 import io.devridge.api.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -29,12 +24,22 @@ public class CourseController {
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(courseList));
     }
 
+//    @GetMapping("/courses/{courseId}")
+//    public ResponseEntity<ApiResponse<Object>> courseDetailList(@PathVariable Long courseId,
+//                                                                @RequestParam("company") Long companyId,
+//                                                                @RequestParam("job") Long jobId) {
+//        CourseDetailResponseDto courseDetailList = courseService.getCourseDetailList(courseId, companyId, jobId);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(courseDetailList));
+//    }
+
     @GetMapping("/courses/{courseId}")
     public ResponseEntity<ApiResponse<Object>> courseDetailList(@PathVariable Long courseId,
                                                                 @RequestParam("company") Long companyId,
-                                                                @RequestParam("job") Long jobId) {
-        CourseDetailResponseDto courseDetailList = courseService.getCourseDetailList(courseId, companyId, jobId);
+                                                                @RequestParam("job") Long jobId,
+                                                                @RequestParam("detailedPosition") Long detailedPositionList) {
+//        CourseDetailResponseDto courseDetailList = courseService.getCourseDetailList(courseId, companyId, jobId);
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(courseDetailList));
+        return null;
     }
 }
