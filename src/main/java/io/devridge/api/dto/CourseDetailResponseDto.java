8e906fbfeb0e9d3d5ddded2861d43dcb58383378
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-10 15:25:35 +0900

    feat: CourseService, getCourseDetailList 구현

--- src/main/java/io/devridge/api/dto/CourseDetailResponseDto.java
+++ src/main/java/io/devridge/api/dto/CourseDetailResponseDto.java
@@ -14,18 +14,22 @@ public class CourseDetailResponseDto {
     public CourseDetailResponseDto(String title, List<CourseDetail> courseDetailList) {
         this.title = title;
 
-        List<CourseDetailDto> DuplicatedList = courseDetailList.stream().map(cd -> new CourseDetailDto(cd)).collect(Collectors.toList());
-        List<CourseDetailDto> notDuplicatedList = new ArrayList<>();
-
-        for(CourseDetailDto cd : DuplicatedList) { // 중복 제거
-            if(!notDuplicatedList.contains(cd)){
-                notDuplicatedList.add(cd);
-            }
-        }
-
-        notDuplicatedList = notDuplicatedList = notDuplicatedList.stream()
-                                            .sorted(Comparator.comparing(CourseDetailDto::getName)).collect(Collectors.toList()); // 이름순 정렬
-        this.courseDetails = notDuplicatedList;
+//        List<CourseDetailDto> DuplicatedList = courseDetailList.stream().map(cd -> new CourseDetailDto(cd)).collect(Collectors.toList());
+//        List<CourseDetailDto> notDuplicatedList = new ArrayList<>();
+//
+//        for(CourseDetailDto cd : DuplicatedList) { // 중복 제거
+//            if(!notDuplicatedList.contains(cd)){
+//                notDuplicatedList.add(cd);
+//            }
+//        }
+//
+//        notDuplicatedList = notDuplicatedList = notDuplicatedList.stream()
+//                                            .sorted(Comparator.comparing(CourseDetailDto::getName)).collect(Collectors.toList()); // 이름순 정렬
+//        this.courseDetails = notDuplicatedList;
+        this.courseDetails = courseDetailList.stream()
+                .map(cd -> new CourseDetailDto(cd))
+                .sorted(Comparator.comparing(CourseDetailDto::getName))
+                .collect(Collectors.toList());  // 이름순 정렬
     }
 
     @Getter
@@ -37,13 +41,12 @@ public class CourseDetailResponseDto {
             this.id = courseDetail.getId();
             this.name = courseDetail.getName();
         }
-
-        public boolean equals(Object obj) { // 두 CourseDetailDto가 있을 때, id가 같으면 같은 객체로 판단함
-            if(obj instanceof CourseDetailDto)
-                return id==((CourseDetailDto)obj).getId();
-            else
-                return false;
-        }
+//        public boolean equals(Object obj) { // 두 CourseDetailDto가 있을 때, id가 같으면 같은 객체로 판단함
+//            if(obj instanceof CourseDetailDto)
+//                return id==((CourseDetailDto)obj).getId();
+//            else
+//                return false;
+//        }
     }
 
 }
