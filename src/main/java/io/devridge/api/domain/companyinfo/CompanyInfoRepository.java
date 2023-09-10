1260ff867b9feea8861a2dec93c88d257fc9fb07
송하빈 <songhabin@songhabin-ui-MacBookPro.local>
2023-09-10 15:25:35 +0900

    feat: CourseService, getCourseDetailList 구현

--- src/main/java/io/devridge/api/domain/companyinfo/CompanyInfoRepository.java
+++ src/main/java/io/devridge/api/domain/companyinfo/CompanyInfoRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CompanyInfoRepository extends JpaRepository<CompanyInfo, Long> {
-
     Optional<CompanyInfo> findByCompanyIdAndJobIdAndDetailedPositionId(long companyId, long jobId, long detailedPositionId);
 }
