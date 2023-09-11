024f001e5d156285c0f08deb177844e951f41759
Woosung_Kim <sibu2005@naver.com>
2023-09-11 23:35:59 +0900

    fix : fetch join -> join으로 수정

--- src/main/java/io/devridge/api/domain/companyinfo/DetailedPositionRepository.java
+++ src/main/java/io/devridge/api/domain/companyinfo/DetailedPositionRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface DetailedPositionRepository extends JpaRepository<DetailedPosition, Long> {
 
     @Query("select dp from DetailedPosition dp " +
-            "JOIN FETCH JobDetailedPosition jdp ON jdp.detailedPosition.id = dp.id " +
+            "JOIN JobDetailedPosition jdp ON jdp.detailedPosition.id = dp.id " +
             "where dp.company.id = :companyId and jdp.job.id = :jobId")
     List<DetailedPosition> findByCompanyIdAndJobId(@Param("companyId") long companyId, @Param("jobId") long jobId);
 }
