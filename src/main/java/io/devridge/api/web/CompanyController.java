f040ab19480cc28539f2b3810b2281efbd22e4f2
Woosung_Kim <sibu2005@naver.com>
2023-09-12 14:11:41 +0900

    hotfix : 프론트 http only 쿠키 확인

--- src/main/java/io/devridge/api/web/CompanyController.java
+++ src/main/java/io/devridge/api/web/CompanyController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,20 @@ public class CompanyController {
     private final CompanyService companyService;
 
     @GetMapping("/companies")
-    public ResponseEntity<ApiResponse<Object>> companyList() {
+    public ResponseEntity<ApiResponse<Object>> companyList(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    System.out.println("cookie.getName() = " + cookie.getName());
+                    String cookieValue = cookie.getValue();
+                    System.out.println("cookieValue = " + cookieValue);
+                    // 쿠키 값을 처리하는 로직
+                }
+            }
+        }
+
         CompanyResponseDto companyResponseDto = companyService.companyList();
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(companyResponseDto));
     }
