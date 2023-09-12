e15093c75480c202bddb6f2e834efbbf2145588d
Woosung_Kim <sibu2005@naver.com>
2023-09-12 14:27:40 +0900

    hotfix : 프론트 http only 쿠키 확인

--- src/main/java/io/devridge/api/web/CompanyController.java
+++ src/main/java/io/devridge/api/web/CompanyController.java
@@ -4,6 +4,7 @@ import io.devridge.api.dto.CompanyResponseDto;
 import io.devridge.api.dto.common.ApiResponse;
 import io.devridge.api.service.CompanyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CompanyController {
@@ -21,13 +23,14 @@ public class CompanyController {
     @GetMapping("/companies")
     public ResponseEntity<ApiResponse<Object>> companyList(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
-
+        log.info("들어옴");
         if (cookies != null) {
+            log.info("들어옴2");
             for (Cookie cookie : cookies) {
                 if ("refreshToken".equals(cookie.getName())) {
-                    System.out.println("cookie.getName() = " + cookie.getName());
+                    log.info("cookie.getName() = {}", cookie.getName());
                     String cookieValue = cookie.getValue();
-                    System.out.println("cookieValue = " + cookieValue);
+                    log.info("cookieValue = {}", cookieValue);
                     // 쿠키 값을 처리하는 로직
                 }
             }
