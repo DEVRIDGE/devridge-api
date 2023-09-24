ec6ddf3073a08b6bfe498d725ac60a33c0ccaaaa
Woosung_Kim <sibu2005@naver.com>
2023-09-24 16:48:30 +0900

    feat : 관리자 페이지 시큐리티 설정 분리 및 처리

---
+++ src/main/java/io/devridge/api/config/EncryptionConfig.java
@@ -0,0 +1,14 @@
+package io.devridge.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncryptionConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/io/devridge/api/config/SecurityConfig.java
+++ src/main/java/io/devridge/api/config/SecurityConfig.java
@@ -7,11 +7,15 @@ import io.devridge.api.config.filter.JwtAuthorizationFilter;
 import io.devridge.api.domain.user.UserRepository;
 import io.devridge.api.util.jwt.TokenProcess;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -20,8 +24,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
@@ -34,8 +42,9 @@ public class SecurityConfig {
     private final TokenProcess tokenProcess;
     private final UserRepository userRepository;
 
+    @Order(2)
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         configureDevSettings(http);
 
         http
@@ -60,6 +69,35 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Order(1)
+    @Bean
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http) throws Exception {
+        configureDevSettings(http);
+
+        http
+            .antMatcher("/admin/**")
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource()).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+                .formLogin()
+                    .loginPage("/admin/login")
+                    .loginProcessingUrl("/admin/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/admin")
+                .failureHandler(this::adminFailureHandler).and()
+                .exceptionHandling()
+                    .authenticationEntryPoint(this::adminAuthenticationEntryPointResponseHandler)
+                    .accessDeniedHandler(this::adminAccessDeniedResponseHandler).and()
+                .authorizeRequests()
+                    .antMatchers("/admin/login").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN");
+
+        return http.build();
+    }
+
+
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
@@ -80,4 +118,22 @@ public class SecurityConfig {
     }
 
     private boolean isProfileActive(String profile) { return List.of(environment.getActiveProfiles()).contains(profile); }
+
+    private void adminFailureHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        log.error("request.getRequestURI() = {}, ", request.getRequestURI());
+        log.error("AuthenticationException = {}", exception.getMessage());
+        response.sendRedirect("/admin/login?error=loginFail");
+    }
+
+    private void adminAuthenticationEntryPointResponseHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        log.error("request.getRequestURI() = {}, ", request.getRequestURI());
+        log.error("AuthenticationException = {}", exception.getMessage());
+        response.sendRedirect("/admin/login?error=needLogin");
+    }
+
+    private void adminAccessDeniedResponseHandler(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException {
+        log.error("request.getRequestURI() = {}, ", request.getRequestURI());
+        log.error("AccessDeniedException = {}", exception.getMessage());
+        response.sendRedirect("/admin/login?error=accessDenied");
+    }
 }
