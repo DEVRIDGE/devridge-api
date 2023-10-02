c51141d9d0a74c499e8fd63775eecd2669617d1f
Habin Song <boulce.email@gmail.com>
2023-10-02 21:42:44 +0900

    feat: 회사에 따른 직무 목록 API 구현

--- src/main/java/io/devridge/api/web/JobController.java
+++ src/main/java/io/devridge/api/web/JobController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +16,8 @@ public class JobController {
     private final JobService jobService;
 
     @GetMapping("/jobs")
-    public ResponseEntity<ApiResponse<Object>> jobList() {
-        JobResponseDto jobResponseDto = jobService.jobList();
+    public ResponseEntity<ApiResponse<Object>> jobListByCompanyId(@RequestParam("company") Long companyId) {
+        JobResponseDto jobResponseDto = jobService.jobListByCompanyId(companyId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(jobResponseDto));
     }
 }
