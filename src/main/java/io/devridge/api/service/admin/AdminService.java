ce0771492c2558a934c1265308c7d340499fde79
Woosung_Kim <sibu2005@naver.com>
2023-10-25 17:58:26 +0900

    fix : 병합전 에러부분 주석

--- src/main/java/io/devridge/api/service/admin/AdminService.java
+++ src/main/java/io/devridge/api/service/admin/AdminService.java
@@ -43,13 +43,13 @@ public class AdminService {
 
     @Transactional
     public void deleteCourse(Long courseId) {
-        Course course = courseRepository.findById(courseId).orElseThrow(() -> new CourseNotFoundException("코스를 찾을 수 없습니다."));
-        List<CourseDetail> courseDetailList = courseDetailRepository.findByCourseId(course.getId());
-        for (CourseDetail courseDetail : courseDetailList) {
-            checkCourseVideoAndRequiredAbilityNull(courseDetail.getId());
-        }
-        courseDetailRepository.deleteAll(courseDetailList);
-        courseRepository.delete(course);
+//        Course course = courseRepository.findById(courseId).orElseThrow(() -> new CourseNotFoundException("코스를 찾을 수 없습니다."));
+//        List<CourseDetail> courseDetailList = courseDetailRepository.findByCourseId(course.getId());
+//        for (CourseDetail courseDetail : courseDetailList) {
+//            checkCourseVideoAndRequiredAbilityNull(courseDetail.getId());
+//        }
+//        courseDetailRepository.deleteAll(courseDetailList);
+//        courseRepository.delete(course);
     }
 
     @Transactional
@@ -67,34 +67,34 @@ public class AdminService {
                 .forEach(companyRequiredAbility -> companyRequiredAbility.changeCourseDetail(null));
     }
 
-    @Transactional
-    public void makeRoadmap() {
-        List<CompanyInfoIdDto> allCompanyInfo = companyInfoRepository.findByAllIdWithRoadmap();
-
-        for (CompanyInfoIdDto companyInfoIdDto : allCompanyInfo) {
-            List<Long> requriedDetailIdList = companyRequiredAbilityRepository.findIdsByCompanyInfoId(companyInfoIdDto.getCompanyInfoId());
-
-            if(companyInfoIdDto.getRoadmapId() == null) {
-                // 로드맵이 존재하지 않는 경우
-                List<Roadmap> roadmapList = new ArrayList<>();
-
-                List<Course> courseList = courseRepository.getCourseListByJob(companyInfoIdDto.getJobId());
-
-                // 로드맵에 모든 코스를 추가해야해서 반복문 돌리는데
-                for (Course course : courseList) {
-
-                    MatchingFlag matchingFlag = checkMatched(course, requriedDetailIdList);
-                    Roadmap roadmap = Roadmap.builder().
-                            course(course).
-                            matchingFlag(matchingFlag).
-                            companyInfo(CompanyInfo.builder().id(companyInfoIdDto.getCompanyInfoId()).build()).
-                            build();
-                    roadmapList.add(roadmap);
-                }
-                roadmapRepository.saveAll(roadmapList);
-            }
-        }
-    }
+//    @Transactional
+//    public void makeRoadmap() {
+//        List<CompanyInfoIdDto> allCompanyInfo = companyInfoRepository.findByAllIdWithRoadmap();
+//
+//        for (CompanyInfoIdDto companyInfoIdDto : allCompanyInfo) {
+//            List<Long> requriedDetailIdList = companyRequiredAbilityRepository.findIdsByCompanyInfoId(companyInfoIdDto.getCompanyInfoId());
+//
+//            if(companyInfoIdDto.getRoadmapId() == null) {
+//                // 로드맵이 존재하지 않는 경우
+//                List<Roadmap> roadmapList = new ArrayList<>();
+//
+//                List<Course> courseList = courseRepository.getCourseListByJob(companyInfoIdDto.getJobId());
+//
+//                // 로드맵에 모든 코스를 추가해야해서 반복문 돌리는데
+//                for (Course course : courseList) {
+//
+//                    MatchingFlag matchingFlag = checkMatched(course, requriedDetailIdList);
+//                    Roadmap roadmap = Roadmap.builder().
+//                            course(course).
+//                            matchingFlag(matchingFlag).
+//                            companyInfo(CompanyInfo.builder().id(companyInfoIdDto.getCompanyInfoId()).build()).
+//                            build();
+//                    roadmapList.add(roadmap);
+//                }
+//                roadmapRepository.saveAll(roadmapList);
+//            }
+//        }
+//    }
 
     @Transactional
     public void matchRequiredAbilityWithCourseDetailId(Long jobId) {
@@ -105,24 +105,24 @@ public class AdminService {
             List<Course> courseList = courseRepository.findByJobId(companyInfo.getJob().getId());
             String abilityName = processStringCutting(companyRequiredAbility.getName());
 
-            Long matchCourseDetailId = courseDetailCompareAbilityName(courseList, abilityName);
-            if(matchCourseDetailId != null) {
-                companyRequiredAbility.changeCourseDetail(CourseDetail.builder().id(matchCourseDetailId).build());
-            }
+//            Long matchCourseDetailId = courseDetailCompareAbilityName(courseList, abilityName);
+//            if(matchCourseDetailId != null) {
+//                companyRequiredAbility.changeCourseDetail(CourseDetail.builder().id(matchCourseDetailId).build());
+//            }
         }
     }
 
-    private Long courseDetailCompareAbilityName(List<Course> courseList, String abilityName) {
-        for (Course course : courseList) {
-            List<CourseDetail> courseDetailList = courseDetailRepository.findByCourseIdOrderByName(course.getId());
-            for (CourseDetail courseDetail : courseDetailList) {
-                if(abilityName.equals(processStringCutting(courseDetail.getName()))) {
-                    return courseDetail.getId();
-                }
-            }
-        }
-        return null;
-    }
+//    private Long courseDetailCompareAbilityName(List<Course> courseList, String abilityName) {
+//        for (Course course : courseList) {
+//            List<CourseDetail> courseDetailList = courseDetailRepository.findByCourseIdOrderByName(course.getId());
+//            for (CourseDetail courseDetail : courseDetailList) {
+//                if(abilityName.equals(processStringCutting(courseDetail.getName()))) {
+//                    return courseDetail.getId();
+//                }
+//            }
+//        }
+//        return null;
+//    }
 
 
     private String processStringCutting(String name) {
@@ -141,16 +141,16 @@ public class AdminService {
         return result.toString();
     }
 
-    private MatchingFlag checkMatched(Course course, List<Long> requriedDetailIdList) {
-        MatchingFlag matchingFlag = MatchingFlag.NO;
-
-        List<CourseDetail> courseDetailList = courseDetailRepository.findByCourseIdOrderByName(course.getId());
-        for (CourseDetail courseDetail : courseDetailList) {
-            if(requriedDetailIdList.contains(courseDetail.getId())) {
-                matchingFlag = MatchingFlag.YES;
-                break;
-            }
-        }
-        return matchingFlag;
-    }
+//    private MatchingFlag checkMatched(Course course, List<Long> requriedDetailIdList) {
+//        MatchingFlag matchingFlag = MatchingFlag.NO;
+//
+//        List<CourseDetail> courseDetailList = courseDetailRepository.findByCourseIdOrderByName(course.getId());
+//        for (CourseDetail courseDetail : courseDetailList) {
+//            if(requriedDetailIdList.contains(courseDetail.getId())) {
+//                matchingFlag = MatchingFlag.YES;
+//                break;
+//            }
+//        }
+//        return matchingFlag;
+//    }
 }
