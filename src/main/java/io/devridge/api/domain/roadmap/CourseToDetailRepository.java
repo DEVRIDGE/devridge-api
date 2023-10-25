929f43a5295c2f029976101272727622f2770fea
Woosung_Kim <sibu2005@naver.com>
2023-10-25 17:59:04 +0900

    fix : 코스, 코스상세 N:N 관계로 설계 변경에 따른 수정

---
+++ src/main/java/io/devridge/api/domain/roadmap/CourseToDetailRepository.java
@@ -0,0 +1,6 @@
+package io.devridge.api.domain.roadmap;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseToDetailRepository extends JpaRepository<CourseToDetail, Long> {
+}
