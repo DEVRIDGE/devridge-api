c2c4a4d3a79113a8f952ce11ff1d8b4351af2c2e
Woosung_Kim <sibu2005@naver.com>
2023-10-26 21:23:46 +0900

    test : 설계 변경으로 인한 테스트 오류 해결

--- src/test/java/io/devridge/api/domain/roadmap/CourseDetailRepositoryTest.java
+++ src/test/java/io/devridge/api/domain/roadmap/CourseDetailRepositoryTest.java
@@ -20,69 +20,92 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class CourseDetailRepositoryTest {
 
-//    @Autowired
-//    private CompanyInfoRepository companyInfoRepository;
-//    @Autowired
-//    private CourseRepository courseRepository;
-//    @Autowired
-//    private CourseDetailRepository courseDetailRepository;
-//    @Autowired
-//    private CompanyRequiredAbilityRepository companyRequiredAbilityRepository;
-//    @Autowired
-//    private EntityManager em;
-//
-//    @DisplayName("코스 상세와 회사 요구 역량이 매칭되는 코스 상세 id 리스트를 반환한다.")
-//    @Test
-//    void get_matching_course_detail_ids_for_company_ability() {
-//        // given
-//        Course course1 = courseRepository.save(Course.builder().build());
-//        Course course2 = courseRepository.save(Course.builder().build());
-//        CourseDetail courseDetail1 = courseDetailRepository.save(CourseDetail.builder().course(course1).build());
-//        courseDetailRepository.save(CourseDetail.builder().course(course1).build());
-//        CourseDetail courseDetail3 = courseDetailRepository.save(CourseDetail.builder().course(course2).build());
-//        courseDetailRepository.save(CourseDetail.builder().course(course2).build());
-//        CompanyInfo companyInfo = companyInfoRepository.save(CompanyInfo.builder().id(1L).build());
-//        companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().companyInfo(companyInfo).courseDetail(courseDetail1).build());
-//        companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().companyInfo(companyInfo).courseDetail(courseDetail3).build());
-//        em.flush();
-//        em.clear();
-//
-//        // when
-//        List<Long> resultList = courseDetailRepository.getMatchingCourseDetailIdsForCompanyAbility(companyInfo.getId());
-//
-//        // then
-//        assertThat(resultList.size()).isEqualTo(2);
-//        assertThat(resultList.get(0)).isEqualTo(courseDetail1.getId());
-//        assertThat(resultList.get(1)).isEqualTo(courseDetail3.getId());
-//    }
-//
-//    @DisplayName("코스 상세 목록에서 회사 요구 역량과 매칭되는 값에는 회사 요구 역량 값이 있고 아닌 경우는 없다. 정렬의 경우 이름 오름차순으로 정렬된다.")
-//    @Test
-//    void get_course_detail_list_with_ability_by_course_id_order_by_name() {
-//        // given
-//        Course course1 = courseRepository.save(Course.builder().build());
-//        Course course2 = courseRepository.save(Course.builder().build());
-//        CourseDetail courseDetail1 = courseDetailRepository.save(CourseDetail.builder().name("Java").course(course1).build());
-//        CourseDetail courseDetail2 = courseDetailRepository.save(CourseDetail.builder().course(course1).name("C++").build());
-//        CourseDetail courseDetail3 = courseDetailRepository.save(CourseDetail.builder().name("Mysql").course(course2).build());
-//        courseDetailRepository.save(CourseDetail.builder().course(course2).name("MongoDB").build());
-//        CompanyInfo companyInfo = companyInfoRepository.save(CompanyInfo.builder().build());
-//        CompanyRequiredAbility companyRequiredAbility = companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().companyInfo(companyInfo).courseDetail(courseDetail1).build());
-//        companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().companyInfo(companyInfo).courseDetail(courseDetail3).build());
-//        em.flush();
-//        em.clear();
-//        List<Long> filteredCourseDetailIds = List.of(courseDetail1.getId(), courseDetail3.getId());
-//
-//        // when
-//        List<CourseDetailWithAbilityDto> resultList = courseDetailRepository.getCourseDetailListWithAbilityByCourseIdOrderByName(course1.getId(), companyInfo.getId(), filteredCourseDetailIds);
-//
-//        // then
-//        assertThat(resultList.size()).isEqualTo(2);
-//        assertThat(resultList.get(0).getCourseDetailId()).isEqualTo(courseDetail2.getId());
-//        assertThat(resultList.get(0).getCourseDetailName()).isEqualTo("C++");
-//        assertThat(resultList.get(0).getCompanyRequiredAbilityId()).isNull();
-//        assertThat(resultList.get(1).getCourseDetailId()).isEqualTo(courseDetail1.getId());
-//        assertThat(resultList.get(1).getCourseDetailName()).isEqualTo("Java");
-//        assertThat(resultList.get(1).getCompanyRequiredAbilityId()).isEqualTo(companyRequiredAbility.getId());
-//    }
+    @Autowired
+    private CompanyInfoRepository companyInfoRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private CourseDetailRepository courseDetailRepository;
+    @Autowired
+    private CompanyRequiredAbilityRepository companyRequiredAbilityRepository;
+    @Autowired
+    private CourseToDetailRepository courseToDetailRepository;
+    @Autowired
+    private CompanyInfoCompanyRequiredAbilityRepository companyInfoCompanyRequiredAbilityRepository;
+    @Autowired
+    private CourseToDetailRepository courseDetailToRepository;
+    @Autowired
+    private EntityManager em;
+
+    @DisplayName("코스 상세와 회사 요구 역량이 매칭되는 코스 상세 id 리스트를 반환한다.")
+    @Test
+    void get_matching_course_detail_ids_for_company_ability() {
+        // given
+        Course course1 = courseRepository.save(Course.builder().build());
+        Course course2 = courseRepository.save(Course.builder().build());
+        CourseDetail courseDetail1 = courseDetailRepository.save(CourseDetail.builder().id(1L).build());
+        CourseDetail courseDetail2 = courseDetailRepository.save(CourseDetail.builder().id(2L).build());
+        CourseDetail courseDetail3 = courseDetailRepository.save(CourseDetail.builder().id(3L).build());
+        CourseDetail courseDetail4 = courseDetailRepository.save(CourseDetail.builder().id(4L).build());
+        courseToDetailRepository.save(CourseToDetail.builder().course(course1).courseDetail(courseDetail1).build());
+        courseToDetailRepository.save(CourseToDetail.builder().course(course1).courseDetail(courseDetail2).build());
+        courseToDetailRepository.save(CourseToDetail.builder().course(course2).courseDetail(courseDetail3).build());
+        courseToDetailRepository.save(CourseToDetail.builder().course(course2).courseDetail(courseDetail4).build());
+
+        CompanyInfo companyInfo = companyInfoRepository.save(CompanyInfo.builder().id(1L).build());
+        CompanyRequiredAbility companyRequiredAbility1 = companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().courseDetail(courseDetail1).build());
+        CompanyRequiredAbility companyRequiredAbility2 = companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().courseDetail(courseDetail3).build());
+
+        companyInfoCompanyRequiredAbilityRepository.save(CompanyInfoCompanyRequiredAbility.builder().companyInfo(companyInfo).companyRequiredAbility(companyRequiredAbility1).build());
+        companyInfoCompanyRequiredAbilityRepository.save(CompanyInfoCompanyRequiredAbility.builder().companyInfo(companyInfo).companyRequiredAbility(companyRequiredAbility2).build());
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Long> resultList = courseDetailRepository.getMatchingCourseDetailIdsForCompanyAbility(companyInfo.getId());
+
+        // then
+        assertThat(resultList.size()).isEqualTo(2);
+        assertThat(resultList.get(0)).isEqualTo(1L);
+        assertThat(resultList.get(1)).isEqualTo(3L);
+    }
+
+    @DisplayName("코스 상세 목록에서 코스 상세 목록에서 회사 요구 역량과 매칭되는 값에는 회사 요구 역량 값이 있고 아닌 경우는 없다. 정렬의 경우 이름 오름차순으로 정렬된다.")
+    @Test
+    void get_course_detail_list_with_ability_by_course_id_order_by_name() {
+        // given
+        Course course1 = courseRepository.save(Course.builder().build());
+        Course course2 = courseRepository.save(Course.builder().build());
+        CourseDetail courseDetail1 = courseDetailRepository.save(CourseDetail.builder().name("Java").build());
+        CourseDetail courseDetail2 = courseDetailRepository.save(CourseDetail.builder().name("C++").build());
+        CourseDetail courseDetail3 = courseDetailRepository.save(CourseDetail.builder().name("Mysql").build());
+        CourseDetail courseDetail4 = courseDetailRepository.save(CourseDetail.builder().name("MongoDB").build());
+        courseDetailToRepository.save(CourseToDetail.builder().course(course1).courseDetail(courseDetail1).build());
+        courseDetailToRepository.save(CourseToDetail.builder().course(course1).courseDetail(courseDetail2).build());
+        courseDetailToRepository.save(CourseToDetail.builder().course(course2).courseDetail(courseDetail3).build());
+        courseDetailToRepository.save(CourseToDetail.builder().course(course2).courseDetail(courseDetail4).build());
+
+        CompanyInfo companyInfo = companyInfoRepository.save(CompanyInfo.builder().build());
+        CompanyRequiredAbility companyRequiredAbility1 = companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().id(1L).courseDetail(courseDetail1).build());
+        CompanyRequiredAbility companyRequiredAbility2 = companyRequiredAbilityRepository.save(CompanyRequiredAbility.builder().id(2L).courseDetail(courseDetail3).build());
+        companyInfoCompanyRequiredAbilityRepository.save(CompanyInfoCompanyRequiredAbility.builder().companyInfo(companyInfo).companyRequiredAbility(companyRequiredAbility1).build());
+        companyInfoCompanyRequiredAbilityRepository.save(CompanyInfoCompanyRequiredAbility.builder().companyInfo(companyInfo).companyRequiredAbility(companyRequiredAbility2).build());
+        em.flush();
+        em.clear();
+
+        List<Long> filteredCourseDetailIds = List.of(courseDetail1.getId(), courseDetail3.getId());
+
+        // when
+        List<CourseDetailWithAbilityDto> resultList = courseDetailRepository.getCourseDetailListWithAbilityByCourseIdOrderByName(course1.getId(), companyInfo.getId(), filteredCourseDetailIds);
+
+        // then
+        assertThat(resultList.size()).isEqualTo(2);
+        assertThat(resultList.get(0).getCourseDetailId()).isEqualTo(courseDetail2.getId());
+        assertThat(resultList.get(0).getCourseDetailName()).isEqualTo("C++");
+        assertThat(resultList.get(0).getCompanyRequiredAbilityId()).isNull();
+        assertThat(resultList.get(1).getCourseDetailId()).isEqualTo(courseDetail1.getId());
+        assertThat(resultList.get(1).getCourseDetailName()).isEqualTo("Java");
+        assertThat(resultList.get(1).getCompanyRequiredAbilityId()).isEqualTo(companyRequiredAbility1.getId());
+    }
 }
--- src/test/java/io/devridge/api/service/CourseVideoServiceTest.java
+++ src/test/java/io/devridge/api/service/CourseVideoServiceTest.java
@@ -28,140 +28,138 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CourseVideoServiceTest {
+    @InjectMocks
+    private CourseVideoService courseVideoService;
+    @Mock
+    private CourseToDetailRepository courseToDetailRepository;
+    @Mock
+    private CourseVideoRepository courseVideoRepository;
+    @Mock
+    private CompanyInfoRepository companyInfoRepository;
+    @Mock
+    private RoadmapRepository roadmapRepository;
 
-//    @InjectMocks
-//    private CourseVideoService courseVideoService;
-//
-//    @Mock
-//    private CourseDetailRepository courseDetailRepository;
-//
-//    @Mock
-//    private CourseVideoRepository courseVideoRepository;
-//
-//    @Mock
-//    private CompanyInfoRepository companyInfoRepository;
-//
-//    @Mock
-//    private RoadmapRepository roadmapRepository;
-//
-//    @DisplayName("코스 상세에 해당하는 영상 목록을 성공적으로 가져온다.")
-//    @Test
-//    public void get_course_video_list_success() {
-//        //given
-//        LoginUser loginUser = LoginUser.builder().user(User.builder().id(1L).build()).build();
-//        CompanyInfo companyInfo = makeCompanyInfo();
-//        Course course = Course.builder().id(1L).name("언어").build();
-//        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").course(course).build();
-//        List<CourseVideo> courseVideoList = makeCourseVideoList(courseDetail);
-//
-//        //stub
-//        when(courseDetailRepository.findById(anyLong())).thenReturn(Optional.of(courseDetail));
-//        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.of(companyInfo));
-//        when(courseVideoRepository.findByCourseDetailIdOrderByLikeCntDesc(anyLong())).thenReturn(courseVideoList);
-//
-//        //when
-//        CourseVideoResponseDto courseVideoResponseDto = courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, loginUser);
-//
-//        //then
-//        assertThat(courseVideoResponseDto.getCourseVideos().size()).isEqualTo(2);
-//        assertThat(courseVideoResponseDto.getCourseVideos().get(0).getTitle()).isEqualTo("Java 강의 영상1");
-//        assertThat(courseVideoResponseDto.getCourseVideos().get(1).getTitle()).isEqualTo("Java 강의 영상2");
-//        assertThat(courseVideoResponseDto.getCourseTitle()).isEqualTo("언어");
-//        assertThat(courseVideoResponseDto.getCourseDetailTitle()).isEqualTo("Java");
-//    }
-//
-//    @DisplayName("로그인 안한 유저가 허용된 영상 목록 요청시 성공적으로 가져온다.")
-//    @Test
-//    public void get_allowed_course_video_list_with_not_login_user() {
-//        //given
-//        CompanyInfo companyInfo = makeCompanyInfo();
-//        Course course = Course.builder().id(1L).name("언어").build();
-//        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").course(course).build();
-//        List<Roadmap> roadmapList = new ArrayList<>();
-//        roadmapList.add(Roadmap.builder().id(1L).course(course).build());
-//        List<CourseVideo> courseVideoList = makeCourseVideoList(courseDetail);
-//
-//        //stub
-//        when(courseDetailRepository.findById(anyLong())).thenReturn(Optional.of(courseDetail));
-//        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.of(companyInfo));
-//        when(roadmapRepository.findTop2ByCompanyInfoIdOrderByCourseOrder(anyLong())).thenReturn(roadmapList);
-//        when(courseVideoRepository.findByCourseDetailIdOrderByLikeCntDesc(anyLong())).thenReturn(courseVideoList);
-//
-//        //when
-//        CourseVideoResponseDto courseVideoResponseDto = courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, null);
-//
-//        //then
-//        assertThat(courseVideoResponseDto.getCourseVideos().size()).isEqualTo(2);
-//        assertThat(courseVideoResponseDto.getCourseVideos().get(0).getTitle()).isEqualTo("Java 강의 영상1");
-//        assertThat(courseVideoResponseDto.getCourseVideos().get(1).getTitle()).isEqualTo("Java 강의 영상2");
-//        assertThat(courseVideoResponseDto.getCourseTitle()).isEqualTo("언어");
-//        assertThat(courseVideoResponseDto.getCourseDetailTitle()).isEqualTo("Java");
-//    }
-//
-//    @DisplayName("로그인 안한 유저가 허용되지 않은 영상 목록 요청시 예외를 발생시킨다.")
-//    @Test
-//    public void get_not_allowed_course_video_list_with_not_login_user_throw_exception() {
-//        //given
-//        CompanyInfo companyInfo = makeCompanyInfo();
-//        Course course1 = Course.builder().id(1L).name("언어").build();
-//        Course course3 = Course.builder().id(3L).name("데이터베이스").build();
-//        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").course(course3).build();
-//        List<Roadmap> roadmapList = new ArrayList<>();
-//        roadmapList.add(Roadmap.builder().id(1L).course(course1).build());
-//        List<CourseVideo> courseVideoList = makeCourseVideoList(courseDetail);
-//
-//        //stub
-//        when(courseDetailRepository.findById(anyLong())).thenReturn(Optional.of(courseDetail));
-//        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.of(companyInfo));
-//        when(roadmapRepository.findTop2ByCompanyInfoIdOrderByCourseOrder(anyLong())).thenReturn(roadmapList);
-//
-//        //when & then
-//        assertThatThrownBy(() -> courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, null))
-//                .isInstanceOf(UnauthorizedCourseAccessException.class);
-//    }
-//
-//    @DisplayName("코스 영상 목록을 가져올 때 맞는 코스 상세 정보를 찾을 수 없으면 예외를 발생시킨다.")
-//    @Test
-//    void get_course_video_list_if_not_found_course_detail_throw_exception() {
-//        //given
-//        LoginUser loginUser = LoginUser.builder().user(User.builder().id(1L).build()).build();
-//
-//        //stub
-//        when(courseDetailRepository.findById(anyLong())).thenReturn(Optional.empty());
-//
-//        //when & then
-//        assertThatThrownBy(() -> courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, loginUser))
-//                .isInstanceOf(CourseDetailNotFoundException.class);
-//    }
-//
-//    @DisplayName("영상 목록을 가져올 때 맞는 회사 정보를 찾을 수 없으면 예외를 발생시킨다.")
-//    @Test
-//    void get_course_video_list_if_not_found_company_info_throw_exception() {
-//        //given
-//        LoginUser loginUser = LoginUser.builder().user(User.builder().id(1L).build()).build();
-//        Course course = Course.builder().id(1L).name("언어").build();
-//        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").course(course).build();
-//
-//        //stub
-//        when(courseDetailRepository.findById(anyLong())).thenReturn(Optional.of(courseDetail));
-//        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.empty());
-//
-//        //when & then
-//        assertThatThrownBy(() -> courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, loginUser))
-//                .isInstanceOf(CompanyInfoNotFoundException.class);
-//    }
-//
-//    private List<CourseVideo> makeCourseVideoList(CourseDetail courseDetail) {
-//        List<CourseVideo> courseVideoList = new ArrayList<>();
-//        courseVideoList.add(CourseVideo.builder().id(1L).title("Java 강의 영상1").courseDetail(courseDetail).build());
-//        courseVideoList.add(CourseVideo.builder().id(2L).title("Java 강의 영상2").courseDetail(courseDetail).build());
-//        return courseVideoList;
-//    }
-//
-//    private CompanyInfo makeCompanyInfo() {
-//        Company company = Company.builder().id(1L).name("test company").build();
-//        Job job = Job.builder().id(1L).name("test job").build();
-//        DetailedPosition detailedPosition = DetailedPosition.builder().id(1L).name("test detailed position").build();
-//        return CompanyInfo.builder().id(1L).company(company).job(job).detailedPosition(detailedPosition).build();
-//    }
+    @DisplayName("코스 상세에 해당하는 영상 목록을 성공적으로 가져온다.")
+    @Test
+    public void get_course_video_list_success() {
+        //given
+        LoginUser loginUser = LoginUser.builder().user(User.builder().id(1L).build()).build();
+        CompanyInfo companyInfo = makeCompanyInfo();
+        Course course = Course.builder().id(1L).name("언어").build();
+        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").build();
+        CourseToDetail courseToDetail = CourseToDetail.builder().course(course).courseDetail(courseDetail).build();
+        List<CourseVideo> courseVideoList = makeCourseVideoList(courseDetail);
+
+        //stub
+        when(courseToDetailRepository.findFetchByCourseIdAndCourseDetailId(anyLong(), anyLong())).thenReturn(Optional.of(courseToDetail));
+        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.of(companyInfo));
+        when(courseVideoRepository.findByCourseDetailIdOrderByLikeCntDesc(anyLong())).thenReturn(courseVideoList);
+
+        //when
+        CourseVideoResponseDto courseVideoResponseDto = courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, 1L, loginUser);
+
+        //then
+        assertThat(courseVideoResponseDto.getCourseVideos().size()).isEqualTo(2);
+        assertThat(courseVideoResponseDto.getCourseVideos().get(0).getTitle()).isEqualTo("Java 강의 영상1");
+        assertThat(courseVideoResponseDto.getCourseVideos().get(1).getTitle()).isEqualTo("Java 강의 영상2");
+        assertThat(courseVideoResponseDto.getCourseTitle()).isEqualTo("언어");
+        assertThat(courseVideoResponseDto.getCourseDetailTitle()).isEqualTo("Java");
+    }
+
+    @DisplayName("로그인 안한 유저가 허용된 영상 목록 요청시 성공적으로 가져온다.")
+    @Test
+    public void get_allowed_course_video_list_with_not_login_user() {
+        //given
+        CompanyInfo companyInfo = makeCompanyInfo();
+        Course course = Course.builder().id(1L).name("언어").build();
+        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").build();
+        CourseToDetail courseToDetail = CourseToDetail.builder().course(course).courseDetail(courseDetail).build();
+        List<Roadmap> roadmapList = new ArrayList<>();
+        roadmapList.add(Roadmap.builder().id(1L).course(course).build());
+        List<CourseVideo> courseVideoList = makeCourseVideoList(courseDetail);
+
+        //stub
+        when(courseToDetailRepository.findFetchByCourseIdAndCourseDetailId(anyLong(), anyLong())).thenReturn(Optional.of(courseToDetail));
+        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.of(companyInfo));
+        when(roadmapRepository.findTop2ByCompanyInfoIdOrderByCourseOrder(anyLong())).thenReturn(roadmapList);
+        when(courseVideoRepository.findByCourseDetailIdOrderByLikeCntDesc(anyLong())).thenReturn(courseVideoList);
+
+        //when
+        CourseVideoResponseDto courseVideoResponseDto = courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, 1L, null);
+
+        //then
+        assertThat(courseVideoResponseDto.getCourseTitle()).isEqualTo("언어");
+        assertThat(courseVideoResponseDto.getCourseDetailTitle()).isEqualTo("Java");
+        assertThat(courseVideoResponseDto.getCourseVideos().size()).isEqualTo(2);
+        assertThat(courseVideoResponseDto.getCourseVideos().get(0).getTitle()).isEqualTo("Java 강의 영상1");
+        assertThat(courseVideoResponseDto.getCourseVideos().get(1).getTitle()).isEqualTo("Java 강의 영상2");
+    }
+
+    @DisplayName("로그인 안한 유저가 허용되지 않은 영상 목록 요청시 예외를 발생시킨다.")
+    @Test
+    public void get_not_allowed_course_video_list_with_not_login_user_throw_exception() {
+        //given
+        CompanyInfo companyInfo = makeCompanyInfo();
+        Course course1 = Course.builder().id(1L).name("언어").build();
+        Course course3 = Course.builder().id(3L).name("데이터베이스").build();
+        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Mysql").build();
+        CourseToDetail courseToDetail = CourseToDetail.builder().course(course3).courseDetail(courseDetail).build();
+        List<Roadmap> roadmapList = new ArrayList<>();
+        roadmapList.add(Roadmap.builder().id(1L).course(course1).build());
+
+        //stub
+        when(courseToDetailRepository.findFetchByCourseIdAndCourseDetailId(anyLong(), anyLong())).thenReturn(Optional.of(courseToDetail));
+        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.of(companyInfo));
+        when(roadmapRepository.findTop2ByCompanyInfoIdOrderByCourseOrder(anyLong())).thenReturn(roadmapList);
+
+        //when & then
+        assertThatThrownBy(() -> courseVideoService.getCourseVideoList(3L, 1L, 1L, 1L, 1L, null))
+                .isInstanceOf(UnauthorizedCourseAccessException.class);
+    }
+
+    @DisplayName("코스 영상 목록을 가져올 때 맞는 코스 상세 정보를 찾을 수 없으면 예외를 발생시킨다.")
+    @Test
+    void get_course_video_list_if_not_found_course_detail_throw_exception() {
+        //given
+        LoginUser loginUser = LoginUser.builder().user(User.builder().id(1L).build()).build();
+
+        //stub
+        when(courseToDetailRepository.findFetchByCourseIdAndCourseDetailId(anyLong(), anyLong())).thenReturn(Optional.empty());
+
+        //when & then
+        assertThatThrownBy(() -> courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, 1L, loginUser))
+                .isInstanceOf(CourseDetailNotFoundException.class);
+    }
+
+    @DisplayName("영상 목록을 가져올 때 맞는 회사 정보를 찾을 수 없으면 예외를 발생시킨다.")
+    @Test
+    void get_course_video_list_if_not_found_company_info_throw_exception() {
+        //given
+        LoginUser loginUser = LoginUser.builder().user(User.builder().id(1L).build()).build();
+        Course course = Course.builder().id(1L).name("언어").build();
+        CourseDetail courseDetail = CourseDetail.builder().id(1L).name("Java").build();
+        CourseToDetail courseToDetail = CourseToDetail.builder().course(course).courseDetail(courseDetail).build();
+
+        //stub
+        when(courseToDetailRepository.findFetchByCourseIdAndCourseDetailId(anyLong(), anyLong())).thenReturn(Optional.of(courseToDetail));
+        when(companyInfoRepository.findByCompanyIdAndJobIdAndDetailedPositionId(anyLong(), anyLong(), anyLong())).thenReturn(Optional.empty());
+
+        //when & then
+        assertThatThrownBy(() -> courseVideoService.getCourseVideoList(1L, 1L, 1L, 1L, 1L, loginUser))
+                .isInstanceOf(CompanyInfoNotFoundException.class);
+    }
+
+    private List<CourseVideo> makeCourseVideoList(CourseDetail courseDetail) {
+        List<CourseVideo> courseVideoList = new ArrayList<>();
+        courseVideoList.add(CourseVideo.builder().id(1L).title("Java 강의 영상1").courseDetail(courseDetail).build());
+        courseVideoList.add(CourseVideo.builder().id(2L).title("Java 강의 영상2").courseDetail(courseDetail).build());
+        return courseVideoList;
+    }
+
+    private CompanyInfo makeCompanyInfo() {
+        Company company = Company.builder().id(1L).name("test company").build();
+        Job job = Job.builder().id(1L).name("test job").build();
+        DetailedPosition detailedPosition = DetailedPosition.builder().id(1L).name("test detailed position").build();
+        return CompanyInfo.builder().id(1L).company(company).job(job).detailedPosition(detailedPosition).build();
+    }
 }
