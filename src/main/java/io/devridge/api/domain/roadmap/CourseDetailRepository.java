0d334b5b1a2f2a87e7c38d9331555ff47e27b5e6
Woosung_Kim <sibu2005@naver.com>
2023-10-26 11:46:21 +0900

    fix : sql 수정

--- src/main/java/io/devridge/api/domain/roadmap/CourseDetailRepository.java
+++ src/main/java/io/devridge/api/domain/roadmap/CourseDetailRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourseDetailRepository extends JpaRepository<CourseDetail, Long> {
 
@@ -14,13 +15,13 @@ public interface CourseDetailRepository extends JpaRepository<CourseDetail, Long
             "WHERE cicra.companyInfo.id = :companyInfoId")
     List<Long> getMatchingCourseDetailIdsForCompanyAbility(Long companyInfoId);
 
-    List<CourseDetail> findByNameOrderById(String name);
+    Optional<CourseDetail> findByName(String name);
 
     @Query("SELECT new io.devridge.api.dto.course.CourseDetailWithAbilityDto(cd.id, cd.name, cra.id) " +
             "FROM CourseDetail cd " +
             "LEFT JOIN CourseToDetail ctd ON cd.id = ctd.courseDetail.id " +
             "LEFT JOIN CompanyRequiredAbility cra ON cd.id = cra.courseDetail.id AND cra.courseDetail.id IN :filteredCourseDetailIds " +
-            "JOIN CompanyInfoCompanyRequiredAbility cicra ON cra.id = cicra.companyRequiredAbility.id AND cicra.companyInfo.id = :companyInfoId " +
+            "LEFT JOIN CompanyInfoCompanyRequiredAbility cicra ON cra.id = cicra.companyRequiredAbility.id AND cicra.companyInfo.id = :companyInfoId " +
             "WHERE ctd.course.id = :courseId ORDER BY cd.name")
     List<CourseDetailWithAbilityDto> getCourseDetailListWithAbilityByCourseIdOrderByName(Long courseId, Long companyInfoId, List<Long> filteredCourseDetailIds);
 }
