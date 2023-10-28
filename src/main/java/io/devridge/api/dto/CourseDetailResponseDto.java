98a0c21f405a6cf274d4f2a3620f1249ed188727
Woosung_Kim <sibu2005@naver.com>
2023-10-28 22:12:50 +0900

    fix : courseDetail List 로직 수정 - 값이 비어있는 경우 쿼리 두번 요청, 직관적으로 코드 변경

--- src/main/java/io/devridge/api/dto/CourseDetailResponseDto.java
+++ src/main/java/io/devridge/api/dto/CourseDetailResponseDto.java
@@ -1,8 +1,8 @@
 package io.devridge.api.dto;
 
+import io.devridge.api.domain.roadmap.CourseDetail;
 import io.devridge.api.domain.user.LoginStatus;
 import io.devridge.api.domain.user.StudyStatus;
-import io.devridge.api.dto.course.CourseDetailWithAbilityDto;
 import io.devridge.api.dto.course.UserStudyStatusDto;
 import lombok.Getter;
 
@@ -16,7 +16,7 @@ public class CourseDetailResponseDto {
     private final StudyStatus studyStatus;
     private final List<CourseDetailDto> courseDetails;
 
-    public CourseDetailResponseDto(String title, UserStudyStatusDto userStudyStatusDto, List<CourseDetailWithAbilityDto> courseDetailList) {
+    public CourseDetailResponseDto(String title, UserStudyStatusDto userStudyStatusDto, List<CourseDetail> courseDetailList) {
         this.courseName = title;
         this.loginStatus = userStudyStatusDto.getLoginStatus();
         this.studyStatus = userStudyStatusDto.getStudyStatus();
@@ -30,9 +30,9 @@ public class CourseDetailResponseDto {
         private final Long id;
         private final String name;
 
-        public CourseDetailDto(CourseDetailWithAbilityDto courseDetailWithAbilityDto) {
-            this.id = courseDetailWithAbilityDto.getCourseDetailId();
-            this.name = courseDetailWithAbilityDto.getCourseDetailName();
+        public CourseDetailDto(CourseDetail courseDetail) {
+            this.id = courseDetail.getId();
+            this.name = courseDetail.getName();
         }
     }
 }
