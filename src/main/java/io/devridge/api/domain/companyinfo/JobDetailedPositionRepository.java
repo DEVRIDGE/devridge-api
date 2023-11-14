3c38999c54c6fb3b07bd364050f7f75bff8d8a0b
Woosung_Kim <sibu2005@naver.com>
2023-11-14 16:49:41 +0900

    style : 코드 스타일 변경 및 주석 제거, 필요없는 기능 제거

--- src/main/java/io/devridge/api/domain/companyinfo/JobDetailedPositionRepository.java
+++ src/main/java/io/devridge/api/domain/companyinfo/JobDetailedPositionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface JobDetailedPositionRepository extends JpaRepository<JobDetailedPosition, Long> {
-    Optional<JobDetailedPosition> findByJobIdAndDetailedPositionId(Long jobId, Long detailedPositionId);
+    Optional<JobDetailedPosition> findByJobAndDetailedPosition(Job job, DetailedPosition detailedPosition);
 }
