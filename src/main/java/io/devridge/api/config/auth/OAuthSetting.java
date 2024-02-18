70ff3ed5fd0abf99e7e41bab1c196f48c0eb6ae4
Woosung_Kim <sibu2005@naver.com>
2024-02-18 13:48:40 +0900

    fix: AWS 주소로 임시 변경

--- src/main/java/io/devridge/api/config/auth/OAuthSetting.java
+++ src/main/java/io/devridge/api/config/auth/OAuthSetting.java
@@ -4,6 +4,7 @@ package io.devridge.api.config.auth;
 public class OAuthSetting {
     public static final String DEV_ROOT_URL = "https://localhost:3000/devridge";
     public static final String PUBLISH_ROOT_URL = "https://devridge.dev";
-    public static final String OAUTH2_LOGIN_AFTER_PAGE = PUBLISH_ROOT_URL + "/loginRedirectPage";
-    public static final String OAUTH2_LOGIN_FAIL_PAGE = PUBLISH_ROOT_URL + "/loginFailRedirectPage";
+    public static final String PUBLISH_AWS_URL = "http://ec2-3-38-125-102.ap-northeast-2.compute.amazonaws.com";
+    public static final String OAUTH2_LOGIN_AFTER_PAGE = PUBLISH_AWS_URL + "/loginRedirectPage";
+    public static final String OAUTH2_LOGIN_FAIL_PAGE = PUBLISH_AWS_URL + "/loginFailRedirectPage";
 }
