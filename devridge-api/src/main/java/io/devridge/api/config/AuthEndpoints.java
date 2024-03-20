ef86a639b939d247561f21cc410424aa99b2215e
Woosung_Kim <sibu2005@naver.com>
2024-03-21 01:00:47 +0900

    fix: web 주소 바꾸고 생긴 문제를 시큐리티 주소 변경으로 해결

--- devridge-api/src/main/java/io/devridge/api/config/AuthEndpoints.java
+++ devridge-api/src/main/java/io/devridge/api/config/AuthEndpoints.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 public class AuthEndpoints {
     public static final String[] REQUIRED_STATIC_AUTH_URLS = { "/required_login/test", "/users/channeltalkinfo", "/videos/like"};
     public static final String[] REQUIRED_WILDCARD_AUTH_URLS = { "/required_login/wild_test/**", "/required_login/**/wild_test", "/**/required_login/wild_test", "/courses/**/change-studystatus"};
-    public static final String[] OPTIONAL_STATIC_AUTH_URLS = { "/courses", "/optional_login/test", "/items" };
-    public static final String[] OPTIONAL_WILDCARD_AUTH_URLS = { "/courses/**", "/optional_login/wild_test/**", "/optional_login/**/wild_test", "/**/optional_login/wild_test" };
+    public static final String[] OPTIONAL_STATIC_AUTH_URLS = { "/courses", "/optional_login/test" };
+    public static final String[] OPTIONAL_WILDCARD_AUTH_URLS = { "/courses/**", "/courses/**/coursedetail/**/items", "/optional_login/wild_test/**", "/optional_login/**/wild_test", "/**/optional_login/wild_test" };
 }
